22f0ee54f006ce27154e9a7fe8b61f86e0505cb3
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-05-18 16:22:59 -0400

    update

--- global.js
+++ global.js
@@ -8,3 +8,6 @@ global.setTimeout(() =>{
 const int = setInterval(() => {
     console.log('in the interval'); 
 }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
---
+++ people.js
@@ -0,0 +1,5 @@
+const people = ['Allison', 'Christian', 'Logan', 'Josie',];
+const ages = [24, 26, 13, 9]
+
+
+module.exports = {people, ages};
